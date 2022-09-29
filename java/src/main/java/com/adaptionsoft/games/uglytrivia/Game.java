122996df44f35972ea922feeb8d99f2eed625cba
Siddharth <spatel@lsoc.org>
2022-09-29 08:36:41 -0500

    made a lot of changes and added methods' '

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -1,9 +1,7 @@
 package com.adaptionsoft.games.uglytrivia;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.io.*;
-
 public class Game {
     ArrayList players = new ArrayList();
     int[] places = new int[6];
@@ -38,77 +36,72 @@ public class Game {
 			System.out.println("error");
 		}
 	}
+	
     
     public  Game(){
     	for (int i = 0; i < 50; i++) {
 			popQuestions.addLast("Pop Question " + i);
 			scienceQuestions.addLast(("Science Question " + i));
 			sportsQuestions.addLast(("Sports Question " + i));
-			rockQuestions.addLast(createRockQuestion(i));
+			rockQuestions.addLast("Rock Question " + i);
     	}
     }
-
-	public String createRockQuestion(int index){
-		return "Rock Question " + index;
-	}
 	
 	public boolean isPlayable() {
-		return (howManyPlayers() >= 2);
+		return (players.size() >= 2);
 	}
-
 	public boolean add(String playerName) {
 		
-		
 	    players.add(playerName);
-	    places[howManyPlayers()] = 0;
-	    purses[howManyPlayers()] = 0;
-	    inPenaltyBox[howManyPlayers()] = false;
+	    places[players.size()] = 0;
+	    purses[players.size()] = 0;
+	    inPenaltyBox[players.size()] = false;
 	    
 	    writeToFile(playerName + " was added");
 	    writeToFile("They are player number " + players.size());
 		return true;
 	}
-	
-	public int howManyPlayers() {
-		return players.size();
-	}
 
+	private void adjustPlaces(int roll) {
+		places[currentPlayer] = places[currentPlayer] + roll;
+		if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+	}
+	private void reportPlaces() {
+		writeToFile(players.get(currentPlayer) 
+				+ "'s new location is " 
+				+ places[currentPlayer]);
+		writeToFile("The category is " + currentCategory());
+	}
+	
+
+	
 	public void roll(int roll) {
 		writeToFile(players.get(currentPlayer) + " is the current player");
 		writeToFile("They have rolled a " + roll);
 		
 		if (inPenaltyBox[currentPlayer]) {
-			if (roll % 2 != 0) {
+			if (roll % 2 == 1) {
 				isGettingOutOfPenaltyBox = true;
-				
 				writeToFile(players.get(currentPlayer) + " is getting out of the penalty box");
-				places[currentPlayer] = places[currentPlayer] + roll;
-				if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
 				
-				writeToFile(players.get(currentPlayer) 
-						+ "'s new location is " 
-						+ places[currentPlayer]);
-				writeToFile("The category is " + currentCategory());
+				adjustPlaces(roll);
+				reportPlaces();
 				askQuestion();
-			} else {
-				writeToFile(players.get(currentPlayer) + " is not getting out of the penalty box");
+			}
+			else {
 				isGettingOutOfPenaltyBox = false;
-				}
-			
-		} else {
-		
-			places[currentPlayer] = places[currentPlayer] + roll;
-			if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-			
-			writeToFile(players.get(currentPlayer) 
-					+ "'s new location is " 
-					+ places[currentPlayer]);
-			writeToFile("The category is " + currentCategory());
+				writeToFile(players.get(currentPlayer) + " is not getting out of the penalty box");
+			}
+		}
+		else {
+			adjustPlaces(roll);
+			reportPlaces();
 			askQuestion();
 		}
 		
 	}
 
+	
 	private void askQuestion() {
 		if (currentCategory() == "Pop")
 			writeToFile(popQuestions.removeFirst().toString());
@@ -122,70 +115,54 @@ public class Game {
 	
 	
 	private String currentCategory() {
-		if (places[currentPlayer] == 0) return "Pop";
-		if (places[currentPlayer] == 4) return "Pop";
-		if (places[currentPlayer] == 8) return "Pop";
-		if (places[currentPlayer] == 1) return "Science";
-		if (places[currentPlayer] == 5) return "Science";
-		if (places[currentPlayer] == 9) return "Science";
-		if (places[currentPlayer] == 2) return "Sports";
-		if (places[currentPlayer] == 6) return "Sports";
-		if (places[currentPlayer] == 10) return "Sports";
+		if (places[currentPlayer] % 4 == 0) return "Pop";
+		if ((places[currentPlayer] - 1) % 4 == 0) return "Science";
+		if ((places[currentPlayer] - 2) % 4 == 0) return "Sports";
 		return "Rock";
 	}
 
+	private void incrementAndDisplayPurse() {
+		purses[currentPlayer]++;
+		writeToFile(players.get(currentPlayer) 
+				+ " now has "
+				+ purses[currentPlayer]
+				+ " Gold Coins.");
+	}
+	private void nextPlayer() {
+		currentPlayer++;
+		if (currentPlayer == players.size()) currentPlayer = 0;
+	}
+	private boolean didPlayerWin() {
+		return !(purses[currentPlayer] == 6);
+	}
+	
 	public boolean wasCorrectlyAnswered() {
 		if (inPenaltyBox[currentPlayer]){
 			if (isGettingOutOfPenaltyBox) {
 				writeToFile("Answer was correct!!!!");
-				purses[currentPlayer]++;
-				writeToFile(players.get(currentPlayer) 
-						+ " now has "
-						+ purses[currentPlayer]
-						+ " Gold Coins.");
-				
+				incrementAndDisplayPurse();
 				boolean winner = didPlayerWin();
-				currentPlayer++;
-				if (currentPlayer == players.size()) currentPlayer = 0;
-				
+				nextPlayer();
 				return winner;
 			} else {
-				currentPlayer++;
-				if (currentPlayer == players.size()) currentPlayer = 0;
+				nextPlayer();
 				return true;
 			}
-			
-			
-			
 		} else {
-		
 			writeToFile("Answer was correct!!!!");
-			purses[currentPlayer]++;
-			writeToFile(players.get(currentPlayer) 
-					+ " now has "
-					+ purses[currentPlayer]
-					+ " Gold Coins.");
-			
+			incrementAndDisplayPurse();
 			boolean winner = didPlayerWin();
-			currentPlayer++;
-			if (currentPlayer == players.size()) currentPlayer = 0;
-			
+			nextPlayer();
 			return winner;
 		}
 	}
-	
+
 	public boolean wrongAnswer(){
 		writeToFile("Question was incorrectly answered");
 		writeToFile(players.get(currentPlayer)+ " was sent to the penalty box");
 		inPenaltyBox[currentPlayer] = true;
-		
-		currentPlayer++;
-		if (currentPlayer == players.size()) currentPlayer = 0;
+		nextPlayer();
 		return true;
 	}
-
-
-	private boolean didPlayerWin() {
-		return !(purses[currentPlayer] == 6);
-	}
+	
 }
